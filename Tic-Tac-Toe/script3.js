39d50db1519effdabaa280dc27ea3af56686a3ba
Vamsi <vamsi8341556098@gmail.com>
2023-08-01 17:03:54 +0530

    This is basic commit

---
+++ Tic-Tac-Toe/script3.js
@@ -0,0 +1,220 @@
+let mediaquery=window.matchMedia('(min-width:640px)')
+console.log("I love you too")
+function blak(mq){
+console.log("I love you too")
+if(mq.matches){
+        console.log("I love you")
+        let music=new Audio("ting.mp3")
+let finish=new Audio("gameover.mp3")
+let turn="X"
+console.log('virat is ${turn}')
+function changeturn(){
+    return turn=="X"?"O":"X"
+}
+let gameOver=false
+let oh=new Audio("oh-my.mp3")
+function game(){
+    let arr=[[0,1,2,6,6,0,20],[3,4,5,5,16,0,20],[6,7,8,6,26,0,20],[0,3,6,-6,16,90,20],[1,4,7,6,16,90,20],[2,5,8,16,16,90,20],[0,4,8,0,16,45,30],[6,2,4,0,16,135,30]]
+    let arr2=[[0,1,2,10,12,0,50],[3,4,5,10,32,0,50],[6,7,8,10,57,0,50],[0,3,6,-15,37,90,50],[1,4,7,8,37,90,50],[2,5,8,28,37,90,50],[0,4,8,0,35,45,63],[6,2,4,0,35,135,63]]
+    let boxtext=document.getElementsByClassName("box")
+    Array.from(arr).forEach(element=>{
+        // console.log(boxtext[element[0]].innerHTML+boxtext[element[1]].innerHTML+boxtext[element[2]].innerHTML)
+        if(boxtext[element[0]].innerHTML==boxtext[element[1]].innerHTML &&boxtext[element[0]].innerHTML==boxtext[element[2]].innerHTML && boxtext[element[0]].innerHTML!='' && boxtext[element[1]].innerHTML!=''){
+            console.log("You dont fooled")
+            if(turn=='X'){
+                console.log(turn)
+            document.getElementsByClassName("info")[0].innerHTML="O WON"
+            }
+            else {
+                console.log(turn)
+            document.getElementsByClassName("info")[0].innerHTML="X WON"
+            }
+            document.getElementsByClassName("info")[0].style.textDecoration='underline'
+            //document.getElementsByClassName("info")[0].style
+            document.getElementsByClassName("imgbox")[0].style.display='flex'
+            // document.querySelector("img").style.transition="width 1s ease"
+            document.querySelector(".img1").style.width="200px"
+            // document.querySelector("img").style.transition="width 1s ease"
+            gameOver=true
+            finish.play()
+            document.querySelectorAll(".line")[0].style.width=`${element[6]}`
+            console.log(document.querySelectorAll(".line")[0].style.width=`${element[6]}vw`)
+            document.querySelectorAll(".line")[0].style.transform=`translate(${element[3]}vw,${element[4]}vw) rotate(${element[5]}deg)`
+            console.log(document.querySelectorAll(".line")[0])
+        document.getElementsByClassName("game-info")[0].querySelector(".reset").innerHTML="NEW GAME"
+
+        }
+        
+    })
+    if(boxtext[0].innerHTML!='' && boxtext[1].innerHTML!=''&&boxtext[2].innerHTML!=''&&boxtext[3].innerHTML!=''&&boxtext[4].innerHTML!=''&&boxtext[5].innerHTML!=''&&boxtext[6].innerHTML!=''&&boxtext[7].innerHTML!='' && boxtext[8].innerHTML!='' && gameOver==false){
+        console.log('you fooled')
+         document.getElementsByClassName("info")[0].innerHTML="BOTH LOST"
+         document.getElementsByClassName("info")[0].style.textDecoration='underline'
+        document.getElementsByClassName("imgbox")[0].style.display='flex'
+        // document.querySelector("img").style.transition="width 1s ease"
+        document.querySelector(".img2").style.width="220px"
+        document.getElementsByClassName("game-info")[0].querySelector(".reset").innerHTML="NEW GAME"
+        oh.play()
+     }
+}
+
+//Game logic
+let boxes=document.getElementsByClassName("box")
+Array.from(boxes).forEach(element=>{
+     //let boxtext=element.getElementsByClassName("box")
+    element.addEventListener('click',function(e){
+        if(element.innerHTML==''){
+            //  element.innerHTML=turn
+            if(!gameOver){
+            element.innerHTML=turn
+            turn =changeturn()
+            game()
+            }
+            if(!gameOver && !(boxes[0].innerHTML!='' && boxes[1].innerHTML!=''&&boxes[2].innerHTML!=''&&boxes[3].innerHTML!=''&&boxes[4].innerHTML!=''&&boxes[5].innerHTML!=''&&boxes[6].innerHTML!=''&&boxes[7].innerHTML!='' && boxes[8].innerHTML!='' )){
+                music.play()
+                let info=document.getElementsByClassName("info")
+             info[0].innerHTML="TURN FOR " +turn
+             info[0].style.textDecoration='underline'
+        
+            }
+            
+
+            
+            
+        }
+    })
+})
+let reset=document.getElementsByClassName("game-info")[0].querySelector(".reset")
+reset.addEventListener('click',function(e){
+    let boxess=document.getElementsByClassName("box")
+    Array.from(boxess).forEach(element=>{
+        let boxtext=element.getElementsByClassName("box")
+        element.innerHTML=''
+    })
+    turn='X'
+    let info=document.getElementsByClassName("info")
+    gameOver=false
+    info[0].innerHTML="TURN FOR " +turn
+    info[0].style.textDecoration='underline'
+    //document.getElementsByClassName("imgbox")[0].style.display='none'
+            document.querySelector(".img1").style.width="0px"
+            document.querySelectorAll(".line")[0].style.width=`0`
+        document.getElementsByClassName("game-info")[0].querySelector(".reset").innerHTML="RESTART"
+        document.querySelector(".img2").style.width="0px"
+
+
+})
+console.log("vamsi1")
+console.log(reset)
+console.log(1)
+console.log(boxes)
+}
+else{
+    console.log("I love you")
+        let music=new Audio("ting.mp3")
+let finish=new Audio("gameover.mp3")
+let turn="X"
+console.log('virat is ${turn}')
+function changeturn(){
+    return turn=="X"?"O":"X"
+}
+let gameOver=false
+let oh=new Audio("oh-my.mp3")
+function game(){
+    let arr=[[0,1,2,10,12,0,50],[3,4,5,10,32,0,50],[6,7,8,10,57,0,50],[0,3,6,-15,37,90,50],[1,4,7,8,37,90,50],[2,5,8,28,37,90,50],[0,4,8,0,35,45,63],[6,2,4,0,35,135,63]]
+    let boxtext=document.getElementsByClassName("box")
+    Array.from(arr).forEach(element=>{
+        // console.log(boxtext[element[0]].innerHTML+boxtext[element[1]].innerHTML+boxtext[element[2]].innerHTML)
+        if(boxtext[element[0]].innerHTML==boxtext[element[1]].innerHTML &&boxtext[element[0]].innerHTML==boxtext[element[2]].innerHTML && boxtext[element[0]].innerHTML!='' && boxtext[element[1]].innerHTML!=''){
+            console.log("You dont fooled")
+            if(turn=='X'){
+                console.log(turn)
+            document.getElementsByClassName("info")[0].innerHTML="O WON"
+            }
+            else {
+                console.log(turn)
+            document.getElementsByClassName("info")[0].innerHTML="X WON"
+            }
+            document.getElementsByClassName("info")[0].style.textDecoration='underline'
+            //document.getElementsByClassName("info")[0].style
+            document.getElementsByClassName("imgbox")[0].style.display='flex'
+            // document.querySelector("img").style.transition="width 1s ease"
+            document.querySelector(".img1").style.width="200px"
+            // document.querySelector("img").style.transition="width 1s ease"
+            gameOver=true
+            finish.play()
+            document.querySelectorAll(".line")[0].style.width=`${element[6]}`
+            console.log(document.querySelectorAll(".line")[0].style.width=`${element[6]}vw`)
+            document.querySelectorAll(".line")[0].style.transform=`translate(${element[3]}vw,${element[4]}vw) rotate(${element[5]}deg)`
+            console.log(document.querySelectorAll(".line")[0])
+        document.getElementsByClassName("game-info")[0].querySelector(".reset").innerHTML="NEW GAME"
+
+        }
+        
+    })
+    if(boxtext[0].innerHTML!='' && boxtext[1].innerHTML!=''&&boxtext[2].innerHTML!=''&&boxtext[3].innerHTML!=''&&boxtext[4].innerHTML!=''&&boxtext[5].innerHTML!=''&&boxtext[6].innerHTML!=''&&boxtext[7].innerHTML!='' && boxtext[8].innerHTML!='' && gameOver==false){
+        console.log('you fooled')
+         document.getElementsByClassName("info")[0].innerHTML="BOTH LOST"
+         document.getElementsByClassName("info")[0].style.textDecoration='underline'
+        document.getElementsByClassName("imgbox")[0].style.display='flex'
+        // document.querySelector("img").style.transition="width 1s ease"
+        document.querySelector(".img2").style.width="220px"
+        document.getElementsByClassName("game-info")[0].querySelector(".reset").innerHTML="NEW GAME"
+        oh.play()
+     }
+}
+
+//Game logic
+let boxes=document.getElementsByClassName("box")
+Array.from(boxes).forEach(element=>{
+     //let boxtext=element.getElementsByClassName("box")
+    element.addEventListener('click',function(e){
+        if(element.innerHTML==''){
+            //  element.innerHTML=turn
+            if(!gameOver){
+            element.innerHTML=turn
+            turn =changeturn()
+            game()
+            }
+            if(!gameOver && !(boxes[0].innerHTML!='' && boxes[1].innerHTML!=''&&boxes[2].innerHTML!=''&&boxes[3].innerHTML!=''&&boxes[4].innerHTML!=''&&boxes[5].innerHTML!=''&&boxes[6].innerHTML!=''&&boxes[7].innerHTML!='' && boxes[8].innerHTML!='' )){
+                music.play()
+                let info=document.getElementsByClassName("info")
+             info[0].innerHTML="TURN FOR " +turn
+             info[0].style.textDecoration='underline'
+        
+            }
+            
+
+            
+            
+        }
+    })
+})
+let reset=document.getElementsByClassName("game-info")[0].querySelector(".reset")
+reset.addEventListener('click',function(e){
+    let boxess=document.getElementsByClassName("box")
+    Array.from(boxess).forEach(element=>{
+        let boxtext=element.getElementsByClassName("box")
+        element.innerHTML=''
+    })
+    turn='X'
+    let info=document.getElementsByClassName("info")
+    gameOver=false
+    info[0].innerHTML="TURN FOR " +turn
+    info[0].style.textDecoration='underline'
+    //document.getElementsByClassName("imgbox")[0].style.display='none'
+            document.querySelector(".img1").style.width="0px"
+            document.querySelectorAll(".line")[0].style.width=`0`
+        document.getElementsByClassName("game-info")[0].querySelector(".reset").innerHTML="RESTART"
+        document.querySelector(".img2").style.width="0px"
+
+
+})
+console.log("vamsi1")
+console.log(reset)
+console.log(1)
+console.log(boxes)
+   
+}
+}
+blak(mediaquery)
---
+++ script.js
@@ -0,0 +1,154 @@
+const typed=new Typed(".text_js",{
+    strings: ["a Full Stack Developer","a Programmer","an Enginner","a Full Stack Developr","a Designer"],
+    typeSpeed:150,
+    backSpeed:150,
+    backDelay:1000,
+    loop:true,
+})
+let a=document.getElementsByClassName("read")[0]
+let b=document.getElementsByClassName("more")[0]
+let c=document.getElementsByClassName("about")[0]
+
+let count=0;
+b.addEventListener('click',function(e){
+    if(count%2==0){
+        a.style.display='block'
+        
+    }
+    else{
+        a.style.display='none'
+        // console.log('vamsi'+count)
+    }
+    count++;
+    console.log(c.scrollIntoView({behavior:"smooth"}))
+
+})
+
+function scrollToSection() {
+    
+   console.log(c.scrollIntoView({ behavior: "smooth" }))
+ }
+ function scrollTosection() {
+    let section = document.getElementsByClassName("home")[0];
+   console.log(section)
+  console.log(section.scrollIntoView({ behavior: "smooth" }))
+}
+ function scrollToSection1() {
+     let section = document.getElementsByClassName("skills")[0];
+    console.log(section)
+   console.log(section.scrollIntoView({ behavior: "smooth" }))
+ }
+ function scrollToSection2() {
+    let section = document.getElementsByClassName("project")[0];
+   console.log(section)
+  console.log(section.scrollIntoView({ behavior: "smooth" }))
+}
+function scrollToSection3() {
+    let section = document.getElementsByClassName("contact")[0];
+   console.log(section)
+  console.log(section.scrollIntoView({ behavior: "smooth" }))
+}
+let section=document.querySelectorAll("section");
+let navbar=document.querySelectorAll("nav a");
+
+let menuIcon=document.querySelectorAll("#menu")[0];
+let navi=document.querySelector(".navbar")
+menuIcon.addEventListener('click',function(e){
+    menuIcon.classList.toggle('bx-x')
+    navi.classList.toggle('act')
+})
+let home=navbar[0];
+
+
+window.addEventListener('scroll',()=>  {
+   section.forEach(sec=>{
+    let top = window.scrollY
+    let offset=sec.offsetTop
+    let height=sec.offsetHeight
+    if(top>=offset-height/3){
+    let vd=sec.getAttribute('class')
+        navbar.forEach(nav=>{
+
+            nav.classList.remove('ab')
+            
+            if (nav.getAttribute('href').substring(1) == vd) {
+                nav.classList.add('ab');
+              }
+        })
+        
+    }
+   })
+    menuIcon.classList.remove('bx-x')
+    navi.classList.remove('act')
+})
+
+
+let ad= document.getElementById('form');
+let submitted=document.getElementsByClassName('sendi')[0]
+ad.addEventListener('submit',function(e){
+  submitted.style.opacity=0.5
+  submitted.disabled=true
+    e.preventDefault();
+    let data = {name:e.target[0].value,
+                email:e.target[1].value,
+                mobile_number:e.target[2].value,
+                email_subject:e.target[3].value,
+                message:e.target[4].value}
+    
+    let options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+      }
+    
+    const res =fetch('https://backendportfolio-2nt6.onrender.com/',options)
+
+   
+    .then((res)=>{
+      submitted.style.opacity=1
+      submitted.disabled=false
+      let f=document.getElementsByClassName('sent')[0];
+      function sent(){
+        
+        f.style.height='1em';
+        f.innerHTML='This message has been sent';
+      }
+      let time=setTimeout(sent,0);
+      setTimeout(()=>{
+        
+        f.style.height='0em';
+        f.innerHTML='';
+        console.log('Vamsi is king and you cannot stop him')
+      },5000)
+      for(let k=0;k<5;k++){
+        e.target[k].value=''
+      }
+      console.log(res)
+    })
+    .catch((err)=>{
+      submitted.style.opacity=1
+      submitted.disabled=false
+      let f=document.getElementsByClassName('sent')[0];
+      function sent(){
+        
+        f.style.height='1em';
+        f.style.backgroundColor='red';
+        f.innerHTML='This message has not sent, try again';
+      }
+      let time=setTimeout(sent,0);
+      setTimeout(()=>{
+        
+        f.style.height='0em';
+        f.innerHTML='';
+        
+      },5000)
+      console.error(err)
+    })
+})
+
+let formi=document.getElementsByClassName("formi")[0]
+
+
+
